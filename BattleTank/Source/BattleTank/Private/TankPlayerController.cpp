231f87b0cbabd6e4bc658a8aaadbc15d1d2287f2
Kakemixen <erlingljunggren@hotmail.com>
2018-03-03 13:58:18 +0100

    L_123

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,8 +3,21 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController begin"));
+	ATank* TempTank = GetControlledTank();
+	if (TempTank == nullptr) {
+		UE_LOG(LogTemp, Error, TEXT("No Tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Has Tank %s"), *TempTank->GetName());
+	}
+}
 
-ATank * ATankPlayerController::GetControlledTank() const
+
+ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
